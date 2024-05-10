ad3119f44e6a1e7c5bd08e4ccefd07769679cf60
Ejinomoto <Ejinomoto@users.noreply.github.com>
2024-05-10 09:35:01 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}',
+            './node_modules/flowbite/**/*.js'],
+  theme: {
+    container :{
+      center : true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        'my-image1' : "url('./img/health-screening-76u1zV-a.png')",
+        primary: '#06b6d4',
+        dark: '#1e293b',
+      },
+      screens :{
+        '2xl' : '1320px',
+      },
+      fontFamily :{
+        
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
